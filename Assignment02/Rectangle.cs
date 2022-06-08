406b4e1520833c79cbd5cddcad22b5069448fbb5
Nimrta <nimrta0871@conestogac.on.ca>
2022-06-08 08:39:31 -0500

    The Completed rectangle class

---
+++ Assignment02/Rectangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment02
+{
+   public  class Rectangle
+    {
+        private int length;
+        private int width;
+        public Rectangle()
+        {
+            length = 1;
+            width = 1;
+        }
+        public Rectangle(int length, int width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public int GetLength()
+        {
+            return length;
+        }
+        public int SetLength(int length)
+        {
+            this.length = length;
+            return length;
+        }
+        public int GetWidth()
+        {
+            return width;
+        }
+        public int SetWidth(int width)
+        {
+            this.width = width;
+            return width;
+        }
+        public int GetPerimeter()
+        {
+            int peri = 2 * (length + width);
+            return (peri);
+        }
+        public int GetArea()
+        {
+            int area = (length * width);
+            return (area);
+        }
+
+    }
+}
